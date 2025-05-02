07bfa4ccfdcb17b88c9ee333f4172eaa8c153bef
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-05-02 13:48:13 +0200

    get random choice

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-alert("elo");
+function getComputerChoice()
+{
+    let hands = ['rock', 'paper', 'scissor'];
+
+    return hands[getRandomInteger(0, 2)];
+}
+
+function getRandomInteger(min, max)
+{
+    min = Math.ceil(min);
+    max = Math.floor(max);
+
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
