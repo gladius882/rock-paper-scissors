d2d75628180c691364d0f5f52c234836da9fa400
gladius882 <22199518+gladius882@users.noreply.github.com>
2025-05-09 06:38:26 +0200

    use objects to check game result

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
+const hands = ['rock', 'paper', 'scissors'];
+
+
 function getComputerChoice()
 {
-    let hands = ['rock', 'paper', 'scissor'];
-
-    return hands[getRandomInteger(0, 2)];
+    let hand = hands[getRandomInteger(0, 2)];
+    return HandFactory.create(hand);
 }
 
 function getRandomInteger(min, max)
@@ -13,6 +15,24 @@ function getRandomInteger(min, max)
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function onHandChoice(event)
+{
+    let id = event.currentTarget.id;
+
+    if(hands.includes(id) === false) {
+        alert('Error');
+        return;
+    }
+
+    let playerHand = HandFactory.create(id);
+    let computerHand = getComputerChoice();
+
+    console.log(playerHand);
+    console.log(computerHand);
+
+    console.log(playerHand.gameResultWith(computerHand));
+}
+
 let currentChoice = false;
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -20,9 +40,6 @@ document.addEventListener('DOMContentLoaded', function () {
     let choices = document.getElementsByClassName('player-choice');
 
     for(let i=0; i<choices.length; i++) {
-        choices[i].addEventListener('click', function(event) {
-            
-        });
+        choices[i].addEventListener('click', onHandChoice);
     }
-
 });
